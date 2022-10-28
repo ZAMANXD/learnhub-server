6fdd0d9bfe7c6c11bae8ae919d0310b12a9b9d76
ZAMANXD <znsnlab@gmail.com>
2022-10-28 22:10:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const courses = require('./data.json');
+const port = process.env.PORT || 5000;
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hey! Brain Exercise Server Running');
+});
+app.get('/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send({});
+  res.send(course);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
